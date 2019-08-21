000336adbe4ebdf896f6b00ce82835e429058465
Neology92 <oskarlegner@gmail.com>
2019-08-21 19:53:58 +0200

    Fix comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,9 +40,9 @@ module.exports = {
       options: { files: ['**/*.js'] },
     },
 
-    // {
+    //
     //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
+    //   options:
     //     name: `gatsby-starter-default`,
     //     short_name: `starter`,
     //     start_url: `/`,
@@ -50,9 +50,9 @@ module.exports = {
     //     theme_color: `#663399`,
     //     display: `minimal-ui`,
     //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+    //   ,
+    // ,
+    // this optional plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
