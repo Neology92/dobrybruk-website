e1886eb21ed06ddb328689e70183a413239c4dbe
Neology92 <oskarlegner@gmail.com>
2019-08-21 18:03:03 +0200

    Make menu fixed

--- src/components/Header/HeaderWrapper.js
+++ src/components/Header/HeaderWrapper.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 const HeaderWrapper = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+
   width: 100%;
   height: 80px;
   display: flex;
