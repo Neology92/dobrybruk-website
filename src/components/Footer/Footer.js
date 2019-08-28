c79f21337a63caebd862de26a65090098c5c8dfb
Neology92 <oskarlegner@gmail.com>
2019-08-28 23:54:12 +0200

    Set gatsby manifest

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import logoBW from 'assets/images/logoBW.png';
+import logoBW from 'assets/images/logoBW.svg';
 import FooterWrapper from './FooterWrapper';
 import TextWrapper from './TextWrapper';
+import Icon from '../Icon/Icon';
 import { StyledLink } from './styled';
 
 const Footer = () => {
   return (
     <FooterWrapper>
-      <img src={logoBW} alt="DobryBruk logo" />
+      <StyledIcon src={logoBW} alt="DobryBruk logo" />
       <TextWrapper>
         <div>© 2019 Dobrybruk</div>
         <StyledLink to="/polityka-prywatnosci">Polityka prywatności</StyledLink>
@@ -16,5 +18,12 @@ const Footer = () => {
     </FooterWrapper>
   );
 };
+
+const StyledIcon = styled(Icon)`
+  svg {
+    width: 122px;
+    height: 113px;
+  }
+`;
 
 export default Footer;
