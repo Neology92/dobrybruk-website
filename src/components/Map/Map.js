91c6044dea281b554bbe97876e5d64e4fbe17d19
Neology92 <oskarlegner@gmail.com>
2019-08-28 16:37:05 +0200

    Install google-map-react

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,12 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Map = () => {
-  return <MapWrapper />;
+  return (
+    <MapWrapper>
+      <div />
+    </MapWrapper>
+  );
 };
 
 const MapWrapper = styled.div`
-  background: ${({ theme }) => theme.color.greyL3};
-  width: 60%;
+  display: none;
+
+  ${({ theme }) => theme.media.above.m} {
+    background: ${({ theme }) => theme.color.greyL3};
+    width: 60%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export default Map;
